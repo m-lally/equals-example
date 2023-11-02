46b5c87895515fa375db6cc4b9f4b152f9567f93
Marc Lally <m.lally@pm.me>
2023-11-02 01:27:45 +0000

    added border and pics to test functionality

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -1,12 +1,11 @@
 import React, { FunctionComponent, ReactNode } from "react";
 
 interface Props {
-  text?: string;
   children?: ReactNode;
 }
 
 const Container: FunctionComponent<Props> = (props) => {
-  return <div className="container">{ props.children }</div>;
+  return <div className="container">{props.children}</div>;
 };
 
 export default Container;
